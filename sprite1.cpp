b0e27b19690d9f7d03282dd3fcc07c62a6c3d356
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2018-05-21 20:38:55 -0300

    el gusano responde a las teclas

--- sprite1.cpp
+++ sprite1.cpp
@@ -11,6 +11,7 @@ using std::string;
 
 #define HIGH 1366
 #define WITH 768
+#define UP_BORDER_HIGH 50 
 
 /////////////////////////////////////////////////////////////////////////////////////////
 class Color{
@@ -72,28 +73,28 @@ public:
         this->h = surface->h / rows;
 
     }
-
-
-
-void draw(SDL_Renderer *renderer, SDL_Rect position){
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, this->surface);
-    if (!texture) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
-        return;
-    }
-
-    SDL_Rect dimention = get_dimention();
-    position.h = dimention.h;
-    position.w = dimention.w;
-
-    SDL_RenderCopy(renderer, texture, &dimention,&position);
-    SDL_DestroyTexture(texture);
-    
+void draw(SDL_Surface *screen, int x, int y){
+    SDL_Rect position;
+    position.x = x;
+    position.y = y;
+    draw(screen, position);
 
 }
 
 
-void next_internal_mov(){
+
+void draw(SDL_Surface *screen, SDL_Rect position){
+    SDL_Rect dimention = get_dimention();
+    
+    position.h = dimention.h;
+    position.w = dimention.w;
+    
+    SDL_BlitSurface(this->surface, &dimention, screen, &position);
+             
+}
+
+
+void next_sprite_figure(){
     this->row_num += 1;
     if(this->row_num >= this->columns){
         this->row_num = 0;
@@ -105,88 +106,102 @@ void next_internal_mov(){
 }
 
 
-
-
-
-
 /*~Picture(){
     SDL_FreeSurface(this->surface);
 }*/
 
+};
+
 /////////////////////////////////////////////////////////////////////////////////////////
 
 
-};
+
+/////////////////////////////////////////////////////////////////////////////////////////
 
 class Animation {
     Uint32 timer;
+    int step;
     Picture picture;
     SDL_Rect dimention;
     SDL_Rect position;
+    bool in_movement;
+
+    void move_left(int step){
+        this->position.x -=step;
+    }
+    void move_right(int step){
+        this->position.x +=step;
+    }
+    void move_up(int step){
+        this->position.y +=step;
+    }
+    void move_down(int step){
+        this->position.y -=step;
+    }
+    int next_internal_mov(){
+        this->picture.next_sprite_figure();
+    }
+
 public:
-    Animation(const char * bmp_path, Color color,int columns, int rows,SDL_Rect position, Uint32 timer): 
+    Animation(const char * bmp_path, Color color,int columns, int rows,int x, int y, Uint32 timer): 
     picture(bmp_path, color, columns, rows ){
+        this->step = 0;
         this->timer = timer;
         this->position = position;
+        this->position.x = x;
+        this->position.y = y;
+        this->in_movement = false;
+
+    }
+
+    void draw(SDL_Surface *screen){
+      this->picture.draw(screen, this->position);
+    }
+
+    bool wish_to_move(){
+        this->in_movement = true;
     }
 
     bool is_time_to_move(Uint32 time_passed){
-        return (time_passed > this->timer);
+        return ((time_passed > this->timer) && this->in_movement);
+    }
 
+    void move(){
+        if(this->step == 15){
+            in_movement = false;
+            this->step = 0;
+        }
+        else if(this->step == 14){
+            move_left(10);
+        }
+        next_internal_mov();
+        this->step +=1;
+        
+        
     }
 
 
-void draw(SDL_Renderer *renderer){
-    this->picture.draw(renderer, this->position);
 
-}
-
-void move_left(int step){
-    this->position.x -=step;
-
-}
-
-void move_right(int step){
-    this->position.x +=step;
-
-}
-
-void move_up(int step){
-    this->position.y +=step;
-
-}
-
-void move_down(int step){
-    this->position.y -=step;
-
-}
-
-int next_internal_mov(){
-    this->picture.next_internal_mov();
-}
 
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////
+
 
 
 int main(int argc, char *args[]){
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
+    SDL_Surface *screen;
     SDL_Event event;
-
-  
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
-        return 1;
-    }
-
-    SDL_DisplayMode dm;
-
+    
+    SDL_Init(SDL_INIT_VIDEO); // init video
+   
     //default high and with
     int w = WITH;
     int h = HIGH;
 
+    SDL_DisplayMode dm;
     //getting the actual size of screen computer
     if (SDL_GetDesktopDisplayMode(0, &dm) != 0){
         SDL_Log("SDL_GetDesktopDisplayMode failed: %s", SDL_GetError());
@@ -196,78 +211,80 @@ int main(int argc, char *args[]){
         h = dm.h;
     }
 
-    if (SDL_CreateWindowAndRenderer(w, h, SDL_WINDOW_OPENGL, &window, &renderer)) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
-        return 1;
-    }
+    //creamos la pantalla
+    window = SDL_CreateWindow("WORRRRMSSS", 0, 0, w,h-UP_BORDER_HIGH, 0);
+   
+
+    //tomamos la superficie de la pantalla
+    screen = SDL_GetWindowSurface(window);
 
     //para controlar el tiempo
     Uint32 t0 = SDL_GetTicks();
-    Uint32 t1, time;
+    Uint32 t1;
 
 
-    SDL_Rect position_beam1,position_beam2;
+    Color colorkey_beam(0,255,0);
 
-        Color colorkey_beam(0,255,0);
-
-        SDL_Rect position2;
-
-        position_beam1.x = w/2-20;
-        position_beam1.y = h/2-20;
-
-        Picture beam("viga.bmp", colorkey_beam,1,2);
-        beam.draw(renderer,position_beam1);
-
-        position_beam2.x = 50;
-        position_beam2.y = 50;
-
-        Picture beam2("viga.bmp", colorkey_beam,1,2);
-        beam2.draw(renderer,position_beam2);
-
-        SDL_Rect position_worm;
-        position_worm.x = w/2;
-        position_worm.y = h/2;
-        Color colorkey(128,128,192);
-        Animation personaje(WORM_WALK,colorkey,1,15,position_worm,100);
+    Picture beam("viga.bmp", colorkey_beam,1,2);
+    int position_beam1_x = w/2-100;
+    int position_beam1_y = h/2+40;
+    beam.draw(screen,position_beam1_x,position_beam1_y);
 
 
-        
-    int step = 0;
+    Picture beam2("viga.bmp", colorkey_beam,1,2);
+    int position_beam2_x = 50;
+    int position_beam2_y = 50;
+    beam2.draw(screen, position_beam2_x, position_beam2_y);
 
-    while (true) {
+
+    Color colorkey(128,128,192);
+    Animation personaje(WORM_WALK,colorkey,1,15,w/2,h/2,100);
+    personaje.draw(screen);
+
+ 
+
+
+    while(true){
 
         //control de evento para cerrar ventana
         SDL_PollEvent(&event);
         if (event.type == SDL_QUIT) {
             break;
         }
+        else if(event.type == SDL_KEYDOWN){
+            const Uint8 *state = SDL_GetKeyboardState(NULL);
+            if (state[SDL_SCANCODE_LEFT] ){
+                state = NULL;
+                personaje.wish_to_move();
+            } 
+        }
+
+        //actualiza el dibujo de la superficie en la pantalla
+        SDL_UpdateWindowSurface(window);
 
         // Referencia de tiempo
         t1 = SDL_GetTicks();
-        time = t1 -t0;
-
        
-        if(personaje.is_time_to_move(time)) {
-            step += 1;
+        if(personaje.is_time_to_move(t1 -t0)) {
+            
             // Nueva referencia de tiempo
             t0 = SDL_GetTicks();
-            personaje.next_internal_mov();
-            personaje.move_left(1);
 
-            if(step == 15){
-                step = 0;
-                personaje.move_left(15);
-            }
+            //borro todo lo que estaba
+            //toda la pantalla en negro
+            SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format,0,0,0));
+            
+            //tengo que volver a dibujar todo
+            beam.draw(screen,position_beam1_x,position_beam1_y);
+            beam2.draw(screen,position_beam2_x, position_beam2_y);
+            
             // Movimiento del personaje
-            personaje.draw(renderer);
-
-           SDL_RenderPresent(renderer);
-
-
+            personaje.move();
+            personaje.draw(screen);
         }
-    }
+        
+    } 
 
-    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
 
     SDL_Quit();
