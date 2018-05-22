d460ac9e948e875b60ec46c1a06d4f65bfdd8527
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2018-05-21 23:34:42 -0300

    resources definitions

---
+++ resources_definitions.h
@@ -0,0 +1,23 @@
+#ifndef __RESOURCES_DEFINITIONS_H__
+#define __RESOURCES_DEFINITIONS_H__
+
+
+//un gusano caminando en linea recta
+#define WORM_WALK "resources/worm_walk.bmp"
+#define WORM_WALK_R 128
+#define WORM_WALK_G 128
+#define WORM_WALK_B 192
+#define WORM_WALK_COLUMNS 1
+#define WORM_WALK_ROWS 15
+
+//una viga larga
+#define BIG_BEAM "resources/beam.bmp"
+#define BIG_BEAM_R 0
+#define BIG_BEAM_G 255
+#define BIG_BEAM_B 0
+#define BIG_BEAM_COLUMNS 1
+#define BIG_BEAM_ROWS 2
+
+
+
+#endif
