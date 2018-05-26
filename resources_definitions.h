d6a54b4900d67a0e92bb0bc9814a1ab189adf309
azconita <gabriela.azcona@despegar.com>
2018-05-26 01:00:47 -0300

    un merge raro... main roto

---
+++ resources_definitions.h
@@ -0,0 +1,46 @@
+#ifndef __RESOURCES_DEFINITIONS_H__
+#define __RESOURCES_DEFINITIONS_H__
+
+
+//un gusano caminando en linea recta
+#define WORM_WALK "resources/worm_walk.bmp"
+#define WORM_WALK_R 128
+#define WORM_WALK_G 128
+#define WORM_WALK_B 192
+#define WORM_WALK_COLUMNS 1
+#define WORM_WALK_ROWS 15
+
+//un gusano saltando
+#define WORM_JUMP "resources/worm_jump.bmp"
+#define WORM_JUMP_R 128
+#define WORM_JUMP_G 128
+#define WORM_JUMP_B 192
+#define WORM_JUMP_COLUMNS 1
+#define WORM_JUMP_ROWS 10
+
+//un gusano cayendo
+#define WORM_FALL "resources/worm_fall.bmp"
+#define WORM_FALL_R 128
+#define WORM_FALL_G 128
+#define WORM_FALL_B 192
+#define WORM_FALL_COLUMNS 1
+#define WORM_FALL_ROWS 2
+
+
+//una viga larga
+#define BEAM "resources/beam.bmp"
+#define BEAM_R 0
+#define BEAM_G 255
+#define BEAM_B 0
+#define BEAM_COLUMNS 1
+#define BEAM_ROWS 2
+
+//el agua
+#define WATER "resources/water.bmp"
+#define WATER_R 0
+#define WATER_G 255
+#define WATER_B 0
+#define WATER_COLUMNS 1
+#define WATER_ROWS 4
+
+#endif
