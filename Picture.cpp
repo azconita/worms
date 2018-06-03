4a67a68c54a688b03fb8f486c7874bd10bde09eb
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2018-06-03 02:26:10 -0300

    picture

---
+++ Picture.cpp
@@ -0,0 +1,201 @@
+/*
+ * Picture.cpp
+ *
+ *  Created on: 3 jun. 2018
+ *      Author: jaz
+ */
+
+#include "Picture.h"
+
+SDL_Rect Picture::get_dimention(){
+    SDL_Rect dimention;
+    // Separaciones de 2 píxeles dentro de las rejillas para observar
+    // bien donde empieza una original y donde termina la otra
+    dimention.w = this->w - 2;
+    dimention.h = this->h - 2;
+
+    // Cálculo de la posición de la original // dentro de la rejilla
+
+    dimention.x = (this->column_num * this->w) + 2;
+    dimention.y = (this->row_num * this->h) + 2;
+
+    return dimention;
+}
+
+
+SDL_Surface * Picture::flip(SDL_Surface * original, Uint32 colorkey){
+        SDL_Rect origen;
+        SDL_Rect destino;
+
+        // Origen -> ancho una línea
+        // Comienzo de copia por el principio
+        origen.x = 0;
+        origen.y = 0;
+        origen.w = 1;
+        origen.h = original->h;
+        // Destino -> ancho una lína
+        // Comienzo de 'pegado' por el final
+        // Para lograr la inversión
+        destino.x = original->w;
+        destino.y = 0;
+        destino.w = 1;
+        destino.h = original->h;
+        SDL_Surface *flipped;
+        // Pasamos la imagen a formato de pantalla
+        flipped = SDL_DisplayFormat(original);
+        if(flipped == NULL) {
+            cout << "No podemos convertir la imagen al formato de pantalla" << endl;
+            return NULL;
+        }
+
+        // Preparamos el rectángulo nuevo vacío del color transparente
+        SDL_FillRect(flipped, NULL, colorkey);
+        // Copiamos linea vertical a linea vertical, inversamente
+        for(int i = 0; i < original->w; i++) {
+            SDL_BlitSurface(original, &origen, flipped, &destino);
+            origen.x = origen.x + 1; destino.x = destino.x - 1;
+       }
+       return flipped;
+}
+
+
+
+
+Picture::Picture(const char * bmp_path, Colour color, int columns, int rows){
+        this->rows = rows;
+        this->columns = columns;
+        this->row_num = 0;
+        this->column_num = 0;
+        this->default_direction = Left; //todas las fotos estan para la izquierda
+
+
+        SDL_Surface *tmp = IMG_Load(bmp_path);
+        //SDL_Surface *tmp = SDL_LoadBMP(bmp_path);
+        if (!tmp) {
+            cout <<"Couldn't create surface from image:" << bmp_path << SDL_GetError() << endl;
+            return;
+        }
+
+        this->surface = SDL_DisplayFormat(tmp);
+        SDL_FreeSurface(tmp);
+
+        if(this->surface == NULL) {
+            cout << "Error: " << SDL_GetError() << endl; exit(1);
+        }
+
+        // Calculamos el color transparente, en nuestro caso el verde
+         Uint32 colorkey = SDL_MapRGB(this->surface->format, color.r, color.g, color.b);
+        // Lo establecemos como color transparente
+        SDL_SetColorKey(this->surface, SDL_SRCCOLORKEY, colorkey);
+
+        this->flipped = flip(surface, colorkey);
+
+        // El ancho de una original es el total entre el número de columnas
+        this->w = surface->w / columns;
+        // El alto de una original es el total entre el número de filas
+        this->h = surface->h / rows;
+}
+int Picture::get_height(){
+    return this->h;
+}
+
+int Picture::get_width(){
+    return this->w;
+}
+
+void Picture::draw(SDL_Surface *screen, int x, int y){
+    SDL_Rect position;
+    position.x = x;
+    position.y = y;
+    draw(screen, position, this->default_direction);
+}
+
+
+
+void Picture::draw(SDL_Surface *screen, SDL_Rect position, Direction direction){
+    SDL_Rect dimention = get_dimention();
+
+    position.h = dimention.h;
+    position.w = dimention.w;
+
+    //printf("x = %i, y = %i, h = %i, w = %i \n",position.x, position.y, position.h, position.w );
+
+    if(this->default_direction != direction){
+        SDL_BlitSurface(this->flipped, &dimention, screen, &position);
+        return;
+    }
+
+    SDL_BlitSurface(this->surface, &dimention, screen, &position);
+
+}
+
+
+void Picture::next_sprite_figure(Direction direction){
+    if(this->default_direction != direction){ //hay que usar la inversa
+        this->column_num -=1; // paso al anterior en la misma fila
+        if(this->column_num <= 0){ //si estoy en el primero
+            this->column_num = this->columns - 1; //paso al ultimo
+            this->row_num += 1; //pero de la fila de abajo
+            if(this->row_num >= this->rows){
+                this->row_num = 0; //si estaba en el ultimo vuelvo a empezar
+
+            }
+        }
+        return;
+    }
+
+    this->column_num +=1; // paso al siguiente en la misma fila
+    if(this->column_num >= this->columns){ //si estoy en el ultimo de la fila
+        this->column_num = 0; //paso al primero
+        this->row_num += 1; //pero de la fila de abajo
+        if(this->row_num >= this->rows){
+            this->row_num = 0; //si estaba en el ultimo vuelvo a empezar
+        }
+    }
+}
+
+void Picture::previous_sprite_figure(Direction direction){
+    if(this->default_direction != direction){ //hay que usar la inversa
+        this->column_num +=1; // paso al siguiente en la misma fila
+        if(this->column_num >= this->columns){ //si ya no hay mas
+            this->column_num = 0; //paso al primero
+            this->row_num -= 1; //pero de la fila de arriba
+            if(this->row_num < 0){
+                this->row_num = this->rows - 1; //si estaba en el ultimo vuelvo a empezar
+
+            }
+        }
+        return;
+    }
+
+    this->column_num -=1; // paso al anterior en la misma fila
+    if(this->column_num < 0){ //si ya no hay anterior
+        this->column_num = this->columns - 1; //paso al ultomo
+        this->row_num -= 1; //pero de la fila de arriba
+        if(this->row_num < 0){
+            this->row_num = this->rows - 1; //si estaba en el primero vuelvo al ultimo
+        }
+    }
+}
+
+bool Picture::is_in_first_figure(Direction direction){
+    if(this->default_direction != direction){
+        return (this->row_num <= 0 && this->column_num >= this->columns-1);
+    }
+    return (this->row_num <= 0 && this->column_num <= 0);
+
+}
+
+bool Picture::is_in_last_figure(Direction direction){
+    if(this->default_direction != direction){
+        return (this->row_num >= this->rows-1 && this->column_num <= 0);
+    }
+    return (this->row_num >= this->rows-1 && this->column_num >= this->columns-1);
+
+}
+
+
+Picture::~Picture() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Picture.h
@@ -0,0 +1,44 @@
+/*
+ * Picture.h
+ *
+ *  Created on: 3 jun. 2018
+ *      Author: jaz
+ */
+
+#ifndef PICTURE_H_
+#define PICTURE_H_
+
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <iostream>
+#include "Colour.h"
+#include "DTOs.h"
+
+using std::cout;
+using std::endl;
+
+class Picture {
+	int rows, columns;
+	    int row_num, column_num;
+	    int w, h;
+
+	    Direction default_direction;
+	    SDL_Surface *surface;
+	    SDL_Surface *flipped;
+	SDL_Surface * flip(SDL_Surface * original, Uint32 colorkey);
+	SDL_Rect get_dimention();
+
+public:
+	Picture(const char * bmp_path, Colour color, int columns, int rows);
+	int get_height();
+	int get_width();
+	void draw(SDL_Surface *screen, int x, int y);
+	void draw(SDL_Surface *screen, SDL_Rect position, Direction direction);
+	bool is_in_first_figure(Direction direction);
+	bool is_in_last_figure(Direction direction);
+	void next_sprite_figure(Direction direction);
+	void previous_sprite_figure(Direction direction);
+	virtual ~Picture();
+};
+
+#endif /* PICTURE_H_ */
